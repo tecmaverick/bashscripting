48278562b43df9cbd6eabb535a2f262ad0c8d83d
AbrahamJP <abrahambiz001@gmail.com>
2023-02-10 12:10:18 +1100

    find emails

--- bash/19_grep.sh
+++ bash/19_grep.sh
@@ -1,3 +1,9 @@
+#caseinsensitive Search of instances text "Find" exact match, within current dir and sub dirs
+grep -nri Find
+
+#caseinsensitive Search of instances text starts with "Find" , within current dir and sub dirs
+grep -nri "Find*"
+
 #searches all files within the current directory for the text “framework” and output results with path and line number
 grep -nr "framework" .
 
@@ -13,3 +19,9 @@ grep ~/Official/docs/ -e "kms" -rn --exclude=\*.{json,txt}
 
 # Find all executables
 ls -F | grep '*$'
+
+#Search for emails and output the entire line matching
+grep -nri -E "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" --include=\*.txt
+
+#Search for emails and output only the email address
+grep -nrio -E "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"  --include=\*.txt | cut -d \: -f 3
