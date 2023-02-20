5c35830b6cba7592020944257fcb70a0dfb48de6
AbrahamJP <abrahambiz001@gmail.com>
2023-02-20 11:49:01 +1100

    added newtwork commands to list processes that have active connections to internet

--- bash/23_network.sh
+++ bash/23_network.sh
@@ -1,8 +1,31 @@
-#View all connections
-lsof -i -P
+#List all network connection
+lsof -i
 
-#View list of processes listening to TCP ports
-netstat -ltnp tcp
+#List all open IPv4 connections
+lsof -i 4
 
-#View list of processes listening to UDP ports
-netstat -ltnp udp
+#List all open IPv6 connections
+lsof -i 6
+
+#List process running on specific port
+lsof -i TCP:22
+
+#List all TCP connections
+lsof -i TCP
+
+#List all UDP connections
+lsof -i UDP
+
+#List all open ports from 0-1024
+lsof -i TCP:1-1024
+
+#List all files and network connection opened by processname
+lsof -i | awk '{ if ($1=="ProcessName") { print}}'
+
+#====================================
+#Test if port 80 is used by specific pricess. This listens for command on Port 80
+nc -l 80
+
+# Connect to port 80 from the same machine
+nc localhost 80
+#====================================
